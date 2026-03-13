963d8477ee4b683b9253ec142ad32dc996ecc695
gapuchi <arjun.adhia@gmail.com>
2026-03-12 22:05:54 -0400

    Add Rust Package in Nix

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -9,14 +11,21 @@
       self,
       nixpkgs,
       utils,
+      crane,
     }:
-
     utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        craneLib = crane.mkLib pkgs;
+        mafia-bot = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+          strictDeps = true;
+        };
       in
       {
+        packages.default = mafia-bot;
+
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             cargo
